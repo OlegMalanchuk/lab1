953e242ea507a640ddf0e5fe00df6182478f7193
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-09-24 12:56:52 +0300

    add class Paralle. write logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Paralle paralle = new Paralle(3.3,3,4);
+        paralle.area();
+        System.out.println(paralle.area(3,3,4));
+        paralle.volum();
+        System.out.println(paralle.volum(3,5,3));
     }
 }
